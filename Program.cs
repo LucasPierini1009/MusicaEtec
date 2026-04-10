cd2a39d3a5cfefcc6aa269702339090d2cac1666
ETEC Adolpho Berezin <e107ti@etec.sp.gov.br>
2026-04-10 19:15:31 -0300

    Ponto música

---
+++ Program.cs
@@ -0,0 +1,8 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.ForegroundColor = (ConsoleColor)9;Console.WriteLine("Fio, cê sunce precisar");
+Console.ForegroundColor = (ConsoleColor)9;Console.WriteLine("É só pensar na vóvó");
+Console.ForegroundColor = (ConsoleColor)9;Console.WriteLine("Que ela vai te ajudar");
+Console.ForegroundColor = (ConsoleColor)10;Console.WriteLine("Pensa numa estrada estranha zifio");
+Console.ForegroundColor = (ConsoleColor)10;Console.WriteLine("Lá no seu Jacutá");
+Console.ForegroundColor = (ConsoleColor)10;Console.WriteLine("E numa casinha estranha zifio");
+Console.ForegroundColor = (ConsoleColor)10;Console.WriteLine("Que a vovó tá lá");
